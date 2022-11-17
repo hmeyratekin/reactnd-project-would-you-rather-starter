8ebe5cb7d2fdae70d56752fc66f2cff729679341
Hümeyra Tekin <htekin@kocfinans.com.tr>
2022-11-17 23:50:36 +0300

    Fix question card and avatar url

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,31 +5,17 @@ import Media from "react-bootstrap/Media";
 
 class Question extends Component {
   render() {
-    // console.log(this.props)
-    const { question, authedUser } = this.props;
-    // console.log(authedUser);
+    const { question } = this.props;
     const { name, avatar, timestamp, optionOne, optionTwo } = question;
 
     return (
       <div>
         <Media>
-          <img
-            width={64}
-            height={64}
-            className="mr-3"
-            src={avatar}
-            alt={`Avatar of ${name}`}
-          />
-          <Media.Body>
-            <h5>{name}</h5>
-            <p>{formatDate(timestamp)}</p>
-          </Media.Body>
+          <img width={80} height={80} className="mr-3" src={avatar} alt={`Avatar of ${name}`}/>
+          <Media.Body> <h5>{name}</h5> <p>{formatDate(timestamp)}</p> </Media.Body>
         </Media>
         <hr/>
-        <h4>Would you rather ...</h4>
-        <div>
-
-        </div>
+          <h4>{name} asks would you rather {optionOne.text} or {optionTwo.text}</h4>
         <div>
 
         </div>
@@ -40,10 +26,9 @@ class Question extends Component {
 
 function mapStateToProps({ authedUser, users, questions }, { id }) {
   const question = questions[id];
-  // console.log(authedUser);
   return {
-    authedUser,
-    question: formatQuestion(question, users[question.author], authedUser),
+    question: formatQuestion(question, users[question.author], authedUser)
+
   };
 }
 
