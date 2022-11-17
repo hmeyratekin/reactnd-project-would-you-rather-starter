3080e028975993bfb20eacd5562e8c60ac8c4365
Hümeyra Tekin <htekin@kocfinans.com.tr>
2022-11-18 01:48:32 +0300

    Add necessary files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from "./App";
+import {Provider} from 'react-redux'
+import reducer from './reducers'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {configureStore} from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import logger from "./middleware/logger";
+
+
+const store = configureStore({ reducer: reducer, middleware: [thunk, logger] })
+
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
